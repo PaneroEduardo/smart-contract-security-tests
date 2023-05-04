c016496718cf38049cc4fc0498ff89cee82ade7a
Eduardo Panero Arauzo <eduardo.paneroarauzo@telefonica.com>
2023-05-04 16:27:35 +0200

    Change truffle-config

--- unsafe-bank/truffle-config.js
+++ unsafe-bank/truffle-config.js
@@ -64,11 +64,16 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    local: {
+    development: {
      host: "127.0.0.1",     // Localhost (default: none)
      port: 8545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     },
+    ganache: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",  
+    }
     //
     // An additional network, but with some advanced options…
     // advanced: {
@@ -98,6 +103,7 @@ module.exports = {
     // }
   },
   contracts_directory: "./unsafe-bank/contracts",
+  contracts_build_directory: "./unsafe-bank/build",
 
   // Set default mocha options here, use special reporters, etc.
   mocha: {
